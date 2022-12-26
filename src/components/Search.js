72dc91e2673b940a3574a6f7498fa936cdbf38c8
bornepatrice <patrice.borne@gmail.com>
2022-12-26 17:09:10 +0100

    Intégration MUI

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,45 @@
 import { nanoid } from "nanoid";
 import React, { useEffect, useState } from "react";
-import IsbnService from "../services/IsbnService";
+import axios from "axios";
+import { Container, Card, CardHeader, Grid } from "@mui/material";
 
 const Search = () => {
   const [items, setItems] = useState([]);
   useEffect(() => {
-    let query = IsbnService.search("dans la tete de sherlock");
-    query.then((data) => setItems(data.data.items));
+    axios
+      .get("http://localhost:3001/recette")
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+      .finally(function () {});
   }, []);
   if (items.length === 0) return <div>Aucun résultat</div>;
   return (
-    <div>
-      Hello
-      {items.map((item) => {
-        return <div key={nanoid()}>{item.volumeInfo.title}</div>;
-      })}
-    </div>
+    <Container
+      maxWidth="lg"
+      sx={{ backgroundColor: "white", height: "100%", overflowY: "auto" }}
+    >
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {items.map((item) => {
+          return (
+            <Grid item xs={2} sm={4} md={4}>
+              <Card key={nanoid()} sx={{ maxWidth: 345 }}>
+                <CardHeader
+                  title={`${item.title} pour ${item.nbPersonne} personnes`}
+                />
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
   );
 };
 
